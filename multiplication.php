1386187994aaeb8a10a974ef874c15771d8c9e87
TareKenny <124017324+TareKenny@users.noreply.github.com>
2023-03-28 11:56:23 +0800

    multipliaction tare

---
+++ multiplication.php
@@ -0,0 +1,17 @@
+<hmtl>
+<head><title>MULTIPLICATION AUTOMATION</title>
+</head>
+<body>
+    <center>
+    <h2>MULTIPLIACTION TABLE OF THE BEAITFUL NUMBER 5!</h2>
+    <?php
+        $number = 5;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ."x". $i ."=". $number * $i ."<br>";
+        // echo "werewrreqer";
+        }
+    ?>
+    </center>
+</body>
+</html>
